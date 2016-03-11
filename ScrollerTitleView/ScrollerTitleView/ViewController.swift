ff9288116530fac7a5aac94d93c30ff7e14859c5
liangchuanfei <liangcfly@gmail.com>
2016-03-11 23:15:44 +0800

    UIScrollerView悬停Demo

--- ScrollerTitleView/ScrollerTitleView/ViewController.swift
+++ ScrollerTitleView/ScrollerTitleView/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleScrollerView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-    //    automaticallyAdjustsScrollViewInsets = false
+        automaticallyAdjustsScrollViewInsets = false
         
         setUpTitle()
         setChildViewController()
---
+++ UIScrollerView悬停Demo/UIScrollerView悬停Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIScrollerView悬停Demo
+//
+//  Created by 梁传飞 on 16/3/11.
+//  Copyright © 2016年 梁传飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
