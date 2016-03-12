d3c13ed157033265b85ef8a6fad1493d7ca5f24b
liangchuanfei <liangcfly@gmail.com>
2016-03-12 22:35:16 +0800

    UIScrollerView悬停和下啦放大图片

--- UIScrollerView悬停Demo/UIScrollerView悬停Demo/ViewController.swift
+++ UIScrollerView悬停Demo/UIScrollerView悬停Demo/ViewController.swift
@@ -8,16 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIScrollViewDelegate {
+   
 
+    @IBOutlet weak var scrollerView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var redView: UIView!
+    
+    var imageViewHeight: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollerView.contentSize = CGSizeMake(0,CGRectGetMaxY(self.blueView.frame))
+        scrollerView.delegate = self
+           }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        imageViewHeight = imageView.bounds.size.height
     }
-
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+       
+        if scrollerView.contentOffset.y >= imageViewHeight{
+            var rect = redView.frame
+            rect.origin.y = 0
+            redView.frame = rect
+            view.addSubview(redView)
+            print("----停住")
+        }else{
+            var rect = redView.frame
+            rect.origin.y = imageViewHeight
+            scrollView.addSubview(redView)
+            redView.frame = rect
+            print("----下来")
+
+        }
+        
+//        if scrollerView.contentOffset.y < 0{
+//            let scal = 1 - scrollerView.contentOffset.y / 60
+//            imageView.transform = CGAffineTransformMakeScale(scal, scal)
+//        }
+        
+        var scal = 1 - scrollerView.contentOffset.y / 60
+        if scal < 1{
+            scal = 1
+        }
+            imageView.transform = CGAffineTransformMakeScale(scal, scal)
+        
     }
 
 
