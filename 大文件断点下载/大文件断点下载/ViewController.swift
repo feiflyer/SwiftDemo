7451c43211f580a26a2ab712ecf008cddd651153
liangchuanfei <liangcfly@gmail.com>
2016-03-24 22:26:05 +0800

    文件断点下载

---
+++ 大文件断点下载/大文件断点下载/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  大文件断点下载
+//
+//  Created by 梁传飞 on 16/3/24.
+//  Copyright © 2016年 梁传飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
